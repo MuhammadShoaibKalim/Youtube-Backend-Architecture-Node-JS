2d92fd9be9c7f44bd2d335f69de4b91bfcc7680e
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-14 17:16:08 +0500

    Refactor server configuration for improved readability and maintainability

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from  "dotenv";
 import connectDB from './config/db.js';
+import authRoutes from './routes/auth.route.js';
+import fileUpload from 'express-fileupload';
 dotenv.config();
 const app = express();
 
@@ -10,11 +12,17 @@ const PORT = 4000 || process.env.PORT;
 
 connectDB();
 app.use(express.json());
-// app.use(bodyParser.json())
+app.use(express.urlencoded({
+    extended: true,
+    tempFileDir: '/tmp/',
+}));
+app.use(fileUpload({useTempFiles: true}));
+
 
 //Routes
+app.use("/api/auth", authRoutes);
 
-
+//App listen
 app.listen(PORT, () => {
     console.log(`Server is running on :${PORT}`);
 });
