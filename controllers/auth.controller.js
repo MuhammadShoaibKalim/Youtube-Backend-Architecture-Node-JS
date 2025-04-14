cd97bcf4a9a4dc617d973027656e306b3e2e257e
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-14 17:09:59 +0500

    Implement user registration functionality with logo upload

---
+++ controllers/auth.controller.js
@@ -0,0 +1,58 @@
+import express from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import cloudinary from "../config/cloudinary.js"; 
+import User from "../models/user.model.js";
+
+
+export const register = async (req, res) => {
+  try {
+    const { channelName, phone, password, email } = req.body;
+
+    if (!channelName || !email || !password || !phone || !req.files?.logo) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required including channel logo.",
+      });
+    }
+    console.log("Logo file info:", req.files?.logo);
+
+
+     const salt  = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const uploadedLogo = await cloudinary.uploader.upload(
+      req.files.logo.tempFilePath
+    );
+
+    const newUser = new User({
+      _id: new mongoose.Types.ObjectId(),
+      channelName,
+      phone,
+      email,
+      password: hashedPassword,
+      logoUrl: uploadedLogo.secure_url,
+      logoId: uploadedLogo.public_id,
+    });
+
+    const savedUser = await newUser.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully!",
+      user: {
+        id: savedUser._id,
+        channelName: savedUser.channelName,
+        phone: savedUser.phone,
+        logoUrl: savedUser.logoUrl,
+      },
+    });
+  } catch (error) {
+    console.error("Registration Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error. Registration failed.",
+      error: error.message,
+    });
+  }
+};
