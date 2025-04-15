67a7da9759dd097220dd211c589ea3f50daaafee
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-15 16:48:29 +0500

    Refactor token generation utility for improved functionality and error handling

--- utils/generateToken.util.js
+++ utils/generateToken.util.js
@@ -1,15 +1,34 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
-export const generateToken = (user) => {
-  return jwt.sign(
-    {
+
+export const generateTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    if (!user) throw new ApiError(404, "User not found");
+
+    const payload = {
       _id: user._id,
       channelName: user.channelName,
       email: user.email,
       phone: user.phone,
       logoId: user.logoId,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
+      role:user.role
+    };
+
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "15m",
+    });
+
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "7d",
+    });
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Token generation failed");
+  }
 };
