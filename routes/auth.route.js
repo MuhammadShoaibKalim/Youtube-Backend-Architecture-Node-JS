cefbfb880cac7065e8a64567062add51bdaeab45
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-15 16:48:04 +0500

    Add logout and admin creation routes to authentication module

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { register,login } from "../controllers/auth.controller.js";
+import { register, login, logout, createSuperAdmin, createAdmin } from "../controllers/auth.controller.js";
+import { checkAuthentication, checkSuperAdmin } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", register );
+router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
+router.post("/create-super-admin", checkAuthentication, checkSuperAdmin, createSuperAdmin);
+router.post("/create-admin", checkAuthentication, checkSuperAdmin, createAdmin);
 
 export default router;
