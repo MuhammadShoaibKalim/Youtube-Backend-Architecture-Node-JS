dc9067b00e9f49d77f45d4ed0277263402168387
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-15 16:48:20 +0500

    Refactor user routes for improved organization and clarity

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,32 @@
 import express from "express";
-const router = express.Router();
-import { subscribe, updateProfile } from "../controllers/user.controller.js";
-import { checkAuthentication } from "../middleware/auth.middleware.js";
+import {
+  subscribe,
+  unsubscribe,
+  updateProfile,
+  getUserChannel,
+  getCurrentUser,
+  changeUserPassword,
+  getUserWatchHistory,
+  validUser,
+} from "../controllers/user.controller.js";
 
-router.put("/update-profile", checkAuthentication , updateProfile )
-router.post("/subscribe",checkAuthentication , subscribe );
+import {
+  checkAuthentication,
+  checkRole
+} from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+// All users (authenticated)
+router.put("/update-profile", checkAuthentication, updateProfile);//
+router.get("/me", checkAuthentication, getCurrentUser);//
+router.post("/change-password", checkAuthentication, changeUserPassword);//
+router.get("/watch-history", checkAuthentication, getUserWatchHistory);
+router.get("/validate-user", checkAuthentication, validUser);
+router.get("/channel/:id", checkAuthentication, getUserChannel);
+
+// Only users (role-based)
+router.post("/subscribe", checkAuthentication, checkRole("user"), subscribe);
+router.post("/unsubscribe", checkAuthentication, checkRole("user"), unsubscribe);
 
 export default router;
