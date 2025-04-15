41a5ff1ac76e5ba847ebd74ea5720432c12fd388
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-04-15 13:18:50 +0500

    Add ownership check middleware for video resources

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -29,3 +29,24 @@ export const checkAuthentication = async (req, res, next) => {
 };
 
 
+export const checkOwnership = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+
+    if (!video) {
+      return res.status(404).json({ 
+        success:false,
+        error: "Video not found" 
+      });
+    }
+
+    if (video.user_id.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ error: "You don't have permission to perform this action" });
+    }
+
+    next();
+  } catch (error) {
+    console.log("Error checking video ownership", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
